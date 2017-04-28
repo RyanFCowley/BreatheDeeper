f58f61fd2e8c7ca39b6e3b35f656810fc79d29ea
Euan <euan.mitchell65@gmail.com>
2017-04-28 13:46:22 +0100

    testing js alert box

--- main.js
+++ main.js
@@ -198,7 +198,7 @@ function loadJSON(air){
           //set new variable to aqi level
           var airQuality = thing.aqi;
           //return the air quality value
-          return airQuality;
+          window.alert("Level of pollution: " & airQuality);
 
 
         });
